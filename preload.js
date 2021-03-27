c2850c933d880dde29dfce50b40ef45f590ccc8f
Jonathan Candelaria <candelariajr@appstate.edu>
2021-03-27 01:49:37 -0400

    Update preload.js

--- preload.js
+++ preload.js
@@ -2,9 +2,7 @@ const {contextBridge,ipcRenderer} = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
     saveToElectron: (text) => ipcRenderer.send('save', text),
-    storeData: (channel, data) => {
-        ipcRenderer.on(channel, (event, data) => {
-            console.log(data);
-        });
+    storeData: (channel, func) => {
+        ipcRenderer.on(channel, func);
     }
 });
