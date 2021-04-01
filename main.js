78dff153529f34945122817d6f8815e61d0ad2b6
candelariajr <candelariajr@appstate.edu>
2021-03-31 22:36:13 -0400

    Testing multiple window generation

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const {app, BrowserWindow, ipcMain} = electron;
 const path = require('path');
 
 let window = null;
+let window2 = null;
 
 app.on('ready', () => {
     window = new BrowserWindow({
@@ -19,9 +20,26 @@ app.on('ready', () => {
     window.loadFile('index.html')
         .then(() => {})
         .catch(err => console.log(err));
+
+    window2 = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            enableRemoteModule: false,
+            allowRunningInsecureContent: false,
+            experimentalFeatures: false,
+            preload: path.join(__dirname, "preload.js")
+        }
+    });
+
+    window2.loadFile('index2.html')
+        .then(() => {})
+        .catch(err => console.log(err));
+
 });
 
 ipcMain.on('save', (event,text) => {
+    console.log(text);
     setTimeout(()=>{
         event.sender.send('store-data', text);
     }, 1000)
