f8211bfc0b14556dc4be4f0a74fa7f994cfc1628
candelariajr <candelariajr@appstate.edu>
2021-04-06 22:49:36 -0400

    Restructuring. Adding Documentation. Re-saving popups eliminated.

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ const {app, BrowserWindow, ipcMain, dialog} = electron;
 const path = require('path');
 const fs = require('fs');
 
+const applicationTitle = "Text App";
+
+
+
 let window = null;
+let filePath = undefined; //used by save dialog and general save logic in ipcRenderer
+//event 'save'
+
+//proof of concept for multiple windows
 //let window2 = null;
 
 app.on('ready', () => {
@@ -39,39 +47,66 @@ app.on('ready', () => {
     */
 });
 
+//save event handler takes two arguments, the event and the contents of the main textarea
+//in the Electron window
 ipcMain.on('save', (event,text) => {
     console.log(text);
     //Note this for demonstrative purposes only (custom event handler)
     setTimeout(()=>{
         event.sender.send('store-data', text);
     }, 1000)
-    //save dialog handler
-    dialog.showSaveDialog(window, {
-        title: 'Save Window',
-        defaultPath: 'fileName.txt'
-    }).then(r => {
-        if(!r.canceled){
-            //attempt to write file
-            fs.writeFile(r.filePath,text, (err) => {
-                if(err){
-                    dialog.showMessageBox(window, {
-                        title: 'Text App',
-                        message: 'Error:' + err,
-                    }).then(r => {
-                        console.log(r.response);
-                    });
-                }else{
-                    dialog.showMessageBox(window, {
-                        title: 'Text App',
-                        message: 'Saved successfully!'
-                    }).then(r => {
-                        console.log(r.response);
-                    });
-                }
-            });
-        // user closes save dialog, no save
-        }else{
-            console.log("User cancelled dialog");
-        }
-    });
+    //only go to save dialog handler if this is the first time saving (global filePath not set)
+    if(filePath === undefined){
+        //save dialog handler
+        dialog.showSaveDialog(window, {
+            title: 'Save Window',
+            defaultPath: 'fileName.txt'
+        }).then(r => {
+            if(!r.canceled){
+                //attempt to write file
+                writeFile(r.filePath, text);
+                // user closes save dialog, no save. Electron requires something to be here
+            }else{
+                console.log("User cancelled dialog");
+            }
+        });
+    }else{
+        //attempt to write the file to the global filePath if not set
+        writeFile(text);
+    }
 });
+
+//writes a file given the full filename and contents
+//called by: main when an attempt to write a file is made
+//calls: save
+function writeFile(path, text){
+    fs.promises.writeFile(path, text)
+    .then(() => {
+        save();
+        //set the global file path for this session of the application
+        filePath = path;
+    })
+    .catch(err => {
+        save(err);
+    })
+}
+
+//save function alert dialog. If there is no error message supplied, report successful
+//save of file. If error message is supplied, report it to the user.
+//called by: writeFile
+function save(message = undefined){
+    if(message === undefined){
+        message = "Saved Successfully";
+    }else{
+        message = "Error: " + message;
+    }
+
+    dialog.showMessageBox(window, {
+        title: applicationTitle,
+        message: message
+    }).then(r => {
+        //Electron wants something here, but the program really doesn't care if
+        //the user closed it or not
+        console.log(r.response);
+    });
+}
